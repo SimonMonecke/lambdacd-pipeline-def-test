f62687aa2911171dce222dd1c3207893daf50076
Simon Monecke <Simon.Monecke@otto.de>
2015-10-09 09:30:47 +0200

    refactor test function

--- src/lambdacd_pipeline_def_test/core.clj
+++ src/lambdacd_pipeline_def_test/core.clj
@@ -1,10 +1,24 @@
 (ns lambdacd-pipeline-def-test.core
   (require [clojure.tools.logging :as log]))
 
+(declare test-p-def)
+
+(defn- test-list [l]
+  (every?
+    identity
+    (doall (map test-p-def l))))
+
+(defn- test-symbol [s]
+  (let [r (boolean (resolve s))]
+    (when (not r)
+      (log/error s "is undefinied"))
+    r))
+
+(defn- test-element [x]
+  (or (not (instance? clojure.lang.Symbol x))
+      (test-symbol x)))
+
 (defn test-p-def [x]
   (if (seq? x)
-    (every? identity (doall (map test-p-def x)))
-    (let [r (boolean (resolve x))]
-      (when (not r)
-        (log/error x "is undefinied"))
-      r)))
+    (test-list x)
+    (test-element x)))
