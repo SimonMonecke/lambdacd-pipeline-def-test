da418f04746d4a52899cd0a5cd8d471f9e4e2a91
Simon Monecke <Simon.Monecke@otto.de>
2015-10-09 09:26:24 +0200

    add fields for scm, license, ... in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject lambdacd-pipeline-def-test "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject lambdacd-pipeline-def-test "0.1.0"
+  :description "A Clojure library designed to test whether all the steps in your LambdaCD pipeline definitions are definied."
+  :url "https://github.com/SimonMonecke/lambdacd-pipeline-def-test"
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"}
+  :scm {:name "git"
+        :url "https://github.com/SimonMonecke/lambdacd-pipeline-def-test.git"}
   :main lambdacd-pipeline-def-test.core
+  :test-paths ["test"]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.0"]])
