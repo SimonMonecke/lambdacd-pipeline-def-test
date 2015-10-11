14224ff5a4b79de5899e66c5e255b2bc46be1651
Simon Monecke <SimonMonecke@gmail.com>
2015-10-11 20:18:28 +0200

    update to version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambdacd-pipeline-def-test "0.1.0"
+(defproject lambdacd-pipeline-def-test "0.2.0"
   :description "A Clojure library designed to test whether all the steps in your LambdaCD pipeline definitions are definied."
   :url "https://github.com/SimonMonecke/lambdacd-pipeline-def-test"
   :license {:name "The MIT License (MIT)"
